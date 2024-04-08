541463b11e35816106d4758ddcd07f12b622c32d
Raheel Mughal <raheelmughal018@gmail.com>
2024-04-08 06:08:34 +0500

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-   <main className="min-h-screen antialiased bg-black/[0.96] bg-grid-white/[0.02]">
-    <h1 className="text-2xl text-center">Hello Next!</h1>
-   </main>
+    <main className="min-h-screen antialiased bg-black/[0.96] bg-grid-white/[0.02]">
+      <HeroSection />
+    </main>
   );
 }
